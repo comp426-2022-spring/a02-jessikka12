91af5f6b370b192e70b9178e91052b7d0d528873
Jessica Wang <jessikka@email.unc.edu>
2022-02-08 13:25:48 -0500

    imported

--- flip.js
+++ flip.js
@@ -1,3 +1,4 @@
 // Import the coinFlip function from your coin.mjs file
-
+import * as coin from  "./modules/coin.mjs";
 // Call the coinFlip function and put the return into STDOUT
+console.log(coin.coinFlip());
